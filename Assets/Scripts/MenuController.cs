ffe7e4504d89569b7ef956afbcb3ab6205b27fcb
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2023-11-24 21:01:12 +0100

    Po 10 lekcji

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,23 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] Renderer render;
-    [SerializeField] Slider redSlider, greenSlider, blueSlider;
-    [SerializeField] Color color;
 
+
+    [SerializeField] Renderer carModel;
+    [SerializeField] Slider redSlider, greenSlider, blueSlider;
+    [SerializeField] TextMeshProUGUI redValue, greenValue, blueValue;
+    [SerializeField] Color color;
 
     private void Start()
     {
-        color = IntToColor(
-            PlayerPrefs.GetInt("Red"),
-            PlayerPrefs.GetInt("Green"),
-            PlayerPrefs.GetInt("Blue"));
+        if (PlayerPrefs.HasKey("Red") && PlayerPrefs.HasKey("Green") &&
+            PlayerPrefs.HasKey("Blue"))
+        {
+            color = IntToColor(
+                PlayerPrefs.GetInt("Red"),
+                PlayerPrefs.GetInt("Green"),
+                PlayerPrefs.GetInt("Blue"));
+        }
 
-        render.material.color = color;
+        carModel.material.color = color;
 
         redSlider.value = (int)(color.r * 255f);
         greenSlider.value = (int)(color.g * 255f);
@@ -27,6 +32,11 @@ public class MenuController : MonoBehaviour
     private void Update()
     {
         SetCarColor((int)redSlider.value, (int)greenSlider.value, (int)blueSlider.value);
+
+        redValue.text = redSlider.value.ToString();
+        greenValue.text = greenSlider.value.ToString();
+        blueValue.text = blueSlider.value.ToString();
+
     }
 
     public static Color IntToColor(int red, int green, int blue)
@@ -34,18 +44,20 @@ public class MenuController : MonoBehaviour
         float r = (float)red / 255;
         float g = (float)green / 255;
         float b = (float)blue / 255;
-        
+
         return new Color(r, g, b);
     }
 
     private void SetCarColor(int red, int green, int blue)
     {
         Color carColor = IntToColor(red, green, blue);
-        render.material.color = carColor;
+        carModel.material.color = carColor;
 
         PlayerPrefs.SetInt("Red", red);
         PlayerPrefs.SetInt("Green", green);
         PlayerPrefs.SetInt("Blue", blue);
+
+        
     }
 
 }
