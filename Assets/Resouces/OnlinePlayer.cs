f7919e93924b77bfb05fc6228647fd26fb91af20
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2023-12-08 20:02:41 +0100

    Lekcja 12

---
+++ Assets/Resouces/OnlinePlayer.cs
@@ -0,0 +1,24 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlinePlayer : MonoBehaviourPunCallbacks
+{
+    public static GameObject LocalPlayerInstance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (photonView.IsMine)
+        {
+            LocalPlayerInstance = gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RaceLauncher.cs
+++ Assets/Scripts/RaceLauncher.cs
@@ -1,25 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Realtime;
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
 
-public class RaceLauncher : MonoBehaviour
+public class RaceLauncher : MonoBehaviourPunCallbacks
 {
-    [SerializeField]private TMP_InputField playerName;
-
+    [SerializeField] private TMP_InputField playerName;
+    [SerializeField] private TextMeshProUGUI networkText;
     public const string PLAYER_NAME_KEY = "PlayerName";
 
-    void Start()
+    private byte maxPlayerPerRoom = 3;
+    private bool isConnecting;
+    private const string GAME_VERSION = "2.0";
+
+    void Awake()
     {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
         if (PlayerPrefs.HasKey(PLAYER_NAME_KEY))
         {
             playerName.text = PlayerPrefs.GetString(PLAYER_NAME_KEY);
         }
     }
 
-   public void SetPlayerName(string playerName)
+    public void SetPlayerName(string playerName)
     {
         PlayerPrefs.SetString(PLAYER_NAME_KEY, playerName);
     }
@@ -28,4 +33,55 @@ public class RaceLauncher : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting)
+        {
+            networkText.text += "On Connect to master ... \n";
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+    }
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        networkText.text += "Failed to join Random Room! \n";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = this.maxPlayerPerRoom });
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        networkText.text += "Disconnected because " + cause + "\n";
+        isConnecting = false;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        networkText.text = "Joined room with " + PhotonNetwork.CurrentRoom.PlayerCount +
+            "players.\n";
+        PhotonNetwork.LoadLevel(0);
+    }
+
+    public void ConnectNetwork()
+    {
+        networkText.text = "";
+        isConnecting = true;
+        PhotonNetwork.NickName = playerName.text;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            networkText.text += "Joining room ... \n";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            networkText.text += "Connecting ... \n";
+            PhotonNetwork.GameVersion = GAME_VERSION;
+            PhotonNetwork.ConnectUsingSettings();
+
+        }
+    }
+
 }
